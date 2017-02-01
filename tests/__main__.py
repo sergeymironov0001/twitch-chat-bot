48af7d169bac32898763af671f3a30170b85d2cd
Sergey Mironov <sergeymironov0001@gmail.com>
2017-02-02 00:41:52 +0300

    Fix an issue when unit tests always return 0 status.

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,5 +1,7 @@
+import sys
 import unittest
 
 if __name__ == '__main__':
     all_tests = unittest.TestLoader().discover('./', pattern='*_tests.py')
-    unittest.TextTestRunner().run(all_tests)
+    ret = unittest.TextTestRunner().run(all_tests)
+    sys.exit(not ret.wasSuccessful())
